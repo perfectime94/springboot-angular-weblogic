84631a7f151026bedfdaa4a4066c3ccf493dc561
Lineu Antonio Martins Pereira <lineu-antonio.martins@getronics.com>
2022-06-27 13:21:33 +0200

    modulo backend inicial

---
+++ backend/src/main/java/org/sanidadmadrid/parent/backend/BackendApplication.java
@@ -0,0 +1,15 @@
+package org.sanidadmadrid.parent.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Creado por Lineu Martins el 27/06/2022 13:10
+ */
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+}
