21183f836f83c5a9bb7447ca25e01f6ea430ba39
Lineu Antonio Martins Pereira <lineu-antonio.martins@getronics.com>
2022-07-06 23:00:39 +0200

    backend: Delegar la resolución de rutas a angular

---
+++ backend/src/main/java/org/sanidadmadrid/parent/backend/config/AngularRouting.java
@@ -0,0 +1,33 @@
+package org.sanidadmadrid.parent.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.IOException;
+
+/**
+ * Creado por Lineu Martins el 06/07/2022 22:58
+ */
+@Configuration
+public class AngularRouting implements WebMvcConfigurer {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**")
+				.addResourceLocations("classpath:/static/")
+				.resourceChain(true)
+				.addResolver(new PathResourceResolver() {
+					@Override
+					protected Resource getResource(String resourcePath, Resource location) throws IOException {
+						Resource requestedResource = location.createRelative(resourcePath);
+
+						return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+								: new ClassPathResource("/static/index.html"); // ruta definida en el pom.xml del módulo frontend.
+					}
+				});
+	}
+}
