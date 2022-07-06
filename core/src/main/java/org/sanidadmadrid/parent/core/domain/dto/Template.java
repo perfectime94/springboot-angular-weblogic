18da311008a520f2f48004f700d820e0edca5190
Lineu Antonio Martins Pereira <lineu-antonio.martins@getronics.com>
2022-07-06 22:36:56 +0200

    modulo core inicial

---
+++ core/src/main/java/org/sanidadmadrid/parent/core/domain/dto/Template.java
@@ -0,0 +1,12 @@
+package org.sanidadmadrid.parent.core.domain.dto;
+
+import lombok.Data;
+
+/**
+ * Creado por Lineu Martins el 06/07/2022 21:25
+ */
+@Data
+public class Template {
+	
+	private String templates;
+}
