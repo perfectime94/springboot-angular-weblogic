8440411ad900123b8f24b6ce2cc0804937c67507
Lineu Antonio Martins Pereira <lineu-antonio.martins@getronics.com>
2022-07-06 22:58:07 +0200

    backend: Habilitar cross-origin para frontend
    Se habilita el crossorigin para todos los accesos a /api desde distinto origen del servidor weblogic, es decir cuando se despliegue el front con su servidor embebido con ng serve (localhost:4200)

---
+++ backend/src/main/java/org/sanidadmadrid/parent/backend/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package org.sanidadmadrid.parent.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Creado por Lineu Martins el 06/07/2022 22:53
+ */
+@Configuration
+@Profile("desarrollo")
+public class CorsConfig implements WebMvcConfigurer {
+
+	/*
+	Permite acceder a los recursos del rest api desde el servidor del frontend levantado con ng serve en desarrollo
+	backend localhost:7001 frontend localhost:4200
+	 */
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/api/**")
+				.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+				.exposedHeaders("Authorization");
+	}
+}
