e1cc3806d037da25d0b9fc4b32815ad939c824bf
Lineu Antonio Martins Pereira <lineu-antonio.martins@getronics.com>
2022-07-06 21:52:45 +0200

    backend: Se configura despliegue en weblogic
    * Se cambia el packaging a war.
    * Se excluye tomcat del starter web, para excluir el servlet embebido.
    * Se incluye el javax servlet 3.1.0 (weblogic 12.2.1.4) con scope provided.
    * Se añade el fichero weblogic.xml para definir la preferencia de paquetes y el context-root
    del aplicativo.
    * En la clase de inicio de springboot se extiende de SpringBootServletInitializer y se implementa
    WebApplicationInitializer para la autoconfiguración con servlet externo.

--- backend/src/main/java/org/sanidadmadrid/parent/backend/BackendApplication.java
+++ backend/src/main/java/org/sanidadmadrid/parent/backend/BackendApplication.java
@@ -2,12 +2,14 @@ package org.sanidadmadrid.parent.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
 
 /**
  * Creado por Lineu Martins el 27/06/2022 13:10
  */
 @SpringBootApplication
-public class BackendApplication {
+public class BackendApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
